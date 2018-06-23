2d777bfa5581f2c716699af889c822c932a79721
Netcarver <id.github.com@gzap.org>
2018-06-23 01:31:15 +0100

    Update Address Lines handling.

--- InputfieldStreetAddress.js
+++ InputfieldStreetAddress.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    function resizeInput() {
+    function updateInput() {
         var add                  = 1;
         var len                  = this.value.length;
         var mathlen              = len;
@@ -45,24 +45,64 @@ $(document).ready(function() {
             malformed = false;
         }
         $(this).toggleClass('streetaddress_malformed', malformed);
-    }
 
 
-    function reduceAddress() {
-        // Hide optional fields.
-        $(this).children('.streetaddress_optional').each(function() {
-            val = $(this).val();
-            if (!val.length) {
-                $(this).hide();
-                $(this).next().hide(); // TODO Make this conditional on being a <br>
+        // Show or hide extra street address lines as needed...
+        var field = $(this).attr('data-field');
+        var is_address_1 = 'street_address'   === field;
+        var is_address_2 = 'street_address_2' === field;
+        var is_address_3 = 'street_address_3' === field;
+
+        if (is_address_1 || is_address_2 || is_address_3) {
+
+            var name          = $(this).attr('data-name');
+            var id1, cl2, cl3, id2, id3;
+
+            id1 = "#Inputfield_" + name + "_street_address";
+            cl2 = "." + name + "_street_address_2_element";
+            id2 = "#Inputfield_" + name + "_street_address_2";
+            cl3 = "." + name + "_street_address_3_element";
+            id3 = "#Inputfield_" + name + "_street_address_3";
+
+            var hidden = 'streetaddress_hidden';
+
+            has_content_1 = $(id1).val().length > 0;
+            has_content_2 = $(id2).val().length > 0;
+            has_content_3 = $(id3).val().length > 0;
+
+            // console.log("Field [" + field + "] - has_content[" + has_content + "]");
+            // console.log("Field 1 [" + is_address_1 + "], Field 2 [" + is_address_2 + "], Field 3 [" + is_address_3 + "]");
+            // console.log("Value 1 [" + has_content_1 + "], Value 2 [" + has_content_2 + "], Value 3 [" + has_content_3 + "]");
+
+            if (!has_content_1 && !has_content_2 && !has_content_3) {
+                $(id1).focus();
+                $(cl2).addClass(hidden);
+                $(cl3).addClass(hidden);
+            } else if (!has_content_2 && !has_content_3) {
+                $(cl2).removeClass(hidden);
+                if (is_address_3)
+                    $(id2).focus();
+                $(cl3).addClass(hidden);
+            } else {
+                $(cl2).removeClass(hidden);
+                $(cl3).removeClass(hidden);
             }
-        });
+        }
     }
 
 
-    $('input.streetaddress').on('input', resizeInput);
-    $('select.streetaddress').on('input', resizeInput);
 
+
+    /**
+     * Adjust inputs as user types in the client.
+     */
+    $('input.streetaddress').on('input', updateInput);
+    $('select.streetaddress').on('input', updateInput);
+
+
+    /**
+     * Initialise the country selector.
+     */
     $('.streetaddress_country').selectize({
         closeAfterSelect: true,
         selectOnTab: true,
