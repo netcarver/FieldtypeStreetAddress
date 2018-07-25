da58f2b68135abee2c8be529ee61466837e09982
Netcarver <id.github.com@gzap.org>
2018-07-25 16:42:32 +0100

    Give example in formats_override.php

--- formats_overrides.php
+++ formats_overrides.php
@@ -2,10 +2,12 @@
 
 /**
  * Whatever you define here will override the values from the libaddressinput feed.
+ *
+ * For example, to override the US address format to remove the comma after the City field you'd add this...
+ * 'US'=>[
+ *     'fmt'=>'%N%n%O%n%A%n%C %S %Z',
+ * ],
  */
 
 return [
-    'US'=>[
-        'fmt'=>'%N%n%O%n%A%n%C %S %Z',
-    ],
 ];
